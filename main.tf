c49ead0c039fd2e7c0580733117cb8dcbb19d95a
jcyk94 <jcyk94@gmail.com>
2024-02-21 13:29:29 +0800

    added ec2 and security group

--- main.tf
+++ main.tf
@@ -1,12 +1,104 @@
 locals {
-  topic-name = "update-topic"
+  topic-name = "jolchin-update-topic"
 }
 
 
 #Resource: aws_sns_topic
 resource "aws_sns_topic" "user_updates" {
 #   name = "jolchin-updates-topic"
-name =var.abc
+name ="${local.topic-name}-${var.abc}"
 }
 
 
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  associate_public_ip_address = true
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.basic-security.id]
+
+  tags = {
+    Name = "jolchin-ec2"
+  }
+}
+
+data "aws_vpc" "main-id" {
+  tags = {
+    Name = "main-vpc"  
+}
+}
+
+resource "aws_security_group" "basic-security" {
+
+  name   = "jolchin-securitygroup-allow-http-https-ssh"
+  description = "allow http, https and ssh"
+  # can use data block instead
+#   vpc_id = var.vpc_id
+  vpc_id = data.aws_vpc.main-id.id
+
+    tags = {
+    Name = "jolchin-securitygroup-allow-http-https-ssh"
+  }
+
+
+  #array, mutltiple securtiy group
+
+# ingress =  [{
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allow HTTP traffic from anywhere"
+#   }]
+
+  ingress = [
+  {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = null
+    prefix_list_ids = []
+    security_groups = []
+    self            = null
+    description = "Allow HTTP traffic from anywhere"
+  },
+  {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = null
+    prefix_list_ids = []
+    security_groups = []
+    self            = null
+    description = "Allow HTTPS traffic from anywhere"
+  },
+   {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["121.6.143.225/32"]
+    ipv6_cidr_blocks = null 
+    prefix_list_ids = []
+    security_groups = []
+    self            = null
+    description = "Allow SSH traffic from your IP address"
+   }
+   ]
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+# ingress = []
+# egress = []
+
+
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,29 @@ variable abc {
       description = "my topic - user_updates"
   
 }
+
+variable "vpc_id" {
+  type = string
+  default = "vpc-02c3cea2893bac90f"
+}
+
+variable "ami_id" {
+      type = string
+      default = "ami-0e731c8a588258d0d"
+      description = "t2.micro-us-east-1"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "subnet_id" {
+  type = string
+  default = "subnet-0d647d78d2309afc0"
+}
+
+variable "key_name" {
+  type = string
+  default = "jolchin-keypair"
+}
